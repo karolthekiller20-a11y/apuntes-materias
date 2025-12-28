b0f52d9b9ad98adc2366f60364f1c052950071b0
karolthekiller20-a11y <karolthekiller2.0@gmail.com>
2025-12-28 15:25:30 -0300

    Update script.js

--- Calculadora-química/script.js
+++ Calculadora-química/script.js
@@ -13,4 +13,20 @@ function calcularMolaridad() {
   document.getElementById("resultadoMolaridad").textContent =
     `M = ${molaridad.toFixed(3)} mol/L`;
 }
+function calcularDilucion() {
+  const m1 = parseFloat(document.getElementById("m1").value);
+  const v1 = parseFloat(document.getElementById("v1").value);
+  const m2 = parseFloat(document.getElementById("m2").value);
+
+  if (isNaN(m1) || isNaN(v1) || isNaN(m2) || m2 <= 0) {
+    document.getElementById("resultadoDilucion").textContent =
+      "⚠️ Ingresa valores válidos";
+    return;
+  }
+
+  const v2 = (m1 * v1) / m2;
+
+  document.getElementById("resultadoDilucion").textContent =
+    `V₂ = ${v2.toFixed(3)} L`;
+}
 
