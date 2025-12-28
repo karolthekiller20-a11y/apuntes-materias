ea014a7a011351c35d8f577b3752c2890e22e661
karolthekiller20-a11y <karolthekiller2.0@gmail.com>
2025-12-28 15:20:56 -0300

    Update script.js

--- Calculadora-química/script.js
+++ Calculadora-química/script.js
@@ -1,1 +1,16 @@
+function calcularMolaridad() {
+  const moles = parseFloat(document.getElementById("moles").value);
+  const volumen = parseFloat(document.getElementById("volumen").value);
+
+  if (isNaN(moles) || isNaN(volumen) || volumen <= 0) {
+    document.getElementById("resultadoMolaridad").textContent =
+      "⚠️ Ingresa valores válidos";
+    return;
+  }
+
+  const molaridad = moles / volumen;
+
+  document.getElementById("resultadoMolaridad").textContent =
+    `M = ${molaridad.toFixed(3)} mol/L`;
+}
 
